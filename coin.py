de1f3fe48b1dd9632595c6252bf1fce5daea239c
Max <max.steinberg.99@gmail.com>
2022-11-10 18:25:07 -0500

    finished jumping

--- coin.py
+++ coin.py
@@ -1,7 +1,16 @@
 import random 
+import pygame
 coin = random.randint(1,100)==x and random.randint(1,100)==y
 if player(x,y) == coin(x,y)
     print(x+1)
 
 
 coin.draw()
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+ 
+        self.image = pygame.image.load("Coin.png")
+        self.rect = self.image.get_rect()
+ 
+        self.rect.topleft = pos
